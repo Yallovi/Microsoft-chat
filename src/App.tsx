e908e3de4ddaa2dc02118ffea3c74f4425bf2479
= <v.moskalenko@brightlab.me>
2023-03-27 17:59:39 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,224 +1,90 @@
 /** @format */
 
-import React, { memo, useCallback, useState } from "react";
-import "./App.css";
-import {
-  Get,
-  Login,
-  MgtTemplateProps,
-  Person,
-  SelectedChannel,
-  TeamsChannelPicker,
-} from "@microsoft/mgt-react";
+import { FluentThemeProvider, MessageThread } from "@azure/communication-react";
+import { Get, Login, MgtLogin, Providers } from "@microsoft/mgt-react";
+import { Chat } from "@microsoft/microsoft-graph-types";
+import { FC, useState, useCallback } from "react";
+import ChatListTemplate from "./components/ChatListTemplate";
+import { Person, PersonCardInteraction } from "@microsoft/mgt-react";
 
-import { Chat, ChatMessage } from "@microsoft/microsoft-graph-types";
-import ChatListTemplate from "./components/ChatListTemplate/ChatListTemplate";
-import TeamsThread from "./components/teamsThread/TeamsThread";
+interface IApp {}
 
-function App() {
-  // const [chatId, setChatId] = useState<string>();
-  // const chatSelected = useCallback(
-  //   (e: Chat) => {
-  //     setChatId(e.id);
-  //   },
-  //   [setChatId]
-  // );
+const client = Providers;
+// console.log("client: ", client);
 
-  const [channelId, setChannelId] = useState<string>("");
-  const [teamId, setTeamId] = useState<string>("");
+const App: FC<IApp> = (props) => {
+  const {} = props;
 
-  const handleSelectionChanged = (event: any) => {
-    const details = event?.detail as SelectedChannel[];
+  const [chatId, setChatId] = useState<string>();
 
-    const { channel, team } = details[0];
+  const chatSelected = useCallback(
+    (e: Chat) => {
+      console.log("e: ", e);
+      setChatId(e.id);
+    },
+    [setChatId]
+  );
 
-    if (!team.id || !channel.id) return;
-
-    setTeamId(team?.id);
-    setChannelId(channel?.id);
-  };
-
-  const HandleContent = (props: MgtTemplateProps) => {
-    const ChatMessages = props?.dataContext?.value as ChatMessage[];
-
+  const HandleChat = (props: any) => {
+    const { value } = props?.dataContext;
     return (
-      <>
-        {ChatMessages?.map((chatMessage, i) => {
-          const { from, deletedDateTime, messageType } = chatMessage;
+      <FluentThemeProvider>
+        <MessageThread
+          userId={"14658b23-6a98-4a79-8162-1c3653fc1f54"}
+          messages={value}
+          showMessageDate={true}
+          // disableEditing={chatState.disableEditing}
+          // TODO: Establish how to stop loading more messages and have it load more as part of an infinite scroll
+          // numberOfChatMessagesToReload={chatState.numberOfChatMessagesToReload}
+          // onLoadPreviousChatMessages={chatState.onLoadPreviousChatMessages}
+          // TODO: Messages date rendering is behind beta flag, find out how to enable it
+          // onDisplayDateTimeString={(date: Date) => date.toISOString()}
 
-          if (deletedDateTime && messageType === "systemEventMessage") return;
-          else if (!from?.user?.id) return;
-
-          return (
-            <div key={i} className="teamsContent flex items-start">
-              <div className="teams-message-author-avatar ">
-                <Person
-                  userId={from?.user?.id}
-                  avatarSize={"large"}
-                  fetchImage={true}
-                  personCardInteraction={1}
-                  showPresence={true}></Person>
-              </div>
-              <TeamsThread
-                teamId={teamId}
-                channelId={channelId}
-                chatMessage={chatMessage}
+          // current behavior for re-send is a delete call with the clientMessageId and the a new send call
+          // onDeleteMessage={chatState.onDeleteMessage}
+          // onSendMessage={chatState.onSendMessage}
+          // onUpdateMessage={chatState.onUpdateMessage}
+          // render props
+          onRenderAvatar={(userId?: string) => {
+            console.log("userId=>", userId);
+            return (
+              <Person
+                userId={userId}
+                avatarSize="small"
+                personCardInteraction={PersonCardInteraction.click}
               />
-            </div>
-          );
-        })}
-      </>
+            );
+          }}
+        />
+        {/* <SendBox
+              autoFocus="sendBoxTextField"
+              onSendMessage={chatState.onSendMessage}
+            /> */}
+        {/* <ErrorBar activeErrorMessages={chatState.activeErrorMessages} /> */}
+      </FluentThemeProvider>
     );
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        Mgt Chat test harness
-        <br />
-        <Login />
-      </header>
-
-      <TeamsChannelPicker
-        selectionChanged={handleSelectionChanged}></TeamsChannelPicker>
-      <div>
-        {teamId && channelId && (
-          <Get
-            version="beta"
-            resource={`teams/${teamId}/channels/${channelId}/messages/delta`}>
-            <HandleContent />
+    <>
+      <Login></Login>
+      <div className="flex">
+        <div>
+          <Get resource="me/chats" scopes={["chat.read"]} cacheEnabled={true}>
+            <ChatListTemplate template="default" onSelected={chatSelected} />
           </Get>
-        )}
+        </div>
+        <div className="w-full">
+          {chatId && (
+            <Get
+              resource={`/teams/{team-id}/channels/{channel-id}/threads/{thread-id}`}>
+              <HandleChat />
+            </Get>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
-}
+};
 
 export default App;
-
-// <div className="teams-thread">
-//           <div className="teams-message">
-//             <mgt-person id="mgt-person-author" user-id="{{from.user.id}}">
-//               <template type="default">
-//                 <span className="teams-message-author-title"
-//                   >{{ person.displayName }}
-//                 </span>
-//                 <span class="teams-message-date"
-//                   >{{ formatDate($parent.lastModifiedDateTime)}}</span
-//                 >
-//               </template>
-//             </mgt-person>
-
-//             <div data-props="innerHTML: body.content"></div>
-//           </div>
-
-//           <mgt-get
-//             resource="/teams/{{ currentTeamId }}/channels/{{ currentChannelId }}/messages/{{ id }}/replies"
-//             version="beta"
-//           >
-//             <template data-type="default">
-//               <div data-if="{{ value.length > 0 }}" className="teams-replies"></div>
-//             </template>
-//             <template data-type="value">
-//               <div
-//                 data-if="!deletedDateTime && messageType !== 'systemEventMessage'"
-//                 className="teams-content teams-reply"
-//               >
-//                 <div className="teams-message-reply-author-avatar">
-//                   <mgt-person
-//                     user-id="{{from.user.id}}"
-//                     avatar-size="large"
-//                     view="avatar"
-//                     person-card="hover"
-//                     show-presence="true"
-//                     className="mgt-person-medium"
-//                   >
-//                   </mgt-person>
-//                 </div>
-//                 <div className="teams-message-reply">
-//                   <mgt-person id="mgt-person-author" user-id="{{from.user.id}}">
-//                     <template type="default">
-//                       <span className="teams-message-author-title"
-//                         >{{ person.displayName }}
-//                       </span>
-//                       <span className="teams-message-date"
-//                         >{{ formatDate($parent.lastModifiedDateTime)}}</span
-//                       >
-//                     </template>
-//                   </mgt-person>
-//                   <div data-props="innerHTML: body.content"></div>
-//                 </div>
-//               </div>
-//             </template>
-//           </mgt-get>
-//         </div>
-
-//  <mgt-get id="mgt-teams-channel-messages" version="beta">
-//     <template data-type="value">
-//       <div
-//         data-if="!deletedDateTime && messageType !== 'systemEventMessage'"
-//         class="teams-content"
-//       >
-
-//         <div class="teams-thread">
-//           <div class="teams-message">
-//             <mgt-person id="mgt-person-author" user-id="{{from.user.id}}">
-//               <template type="default">
-//                 <span class="teams-message-author-title"
-//                   >{{ person.displayName }}
-//                 </span>
-//                 <span class="teams-message-date"
-//                   >{{ formatDate($parent.lastModifiedDateTime)}}</span
-//                 >
-//               </template>
-//             </mgt-person>
-
-//             <div data-props="innerHTML: body.content"></div>
-//           </div>
-
-//           <mgt-get
-//             resource="/teams/{{ currentTeamId }}/channels/{{ currentChannelId }}/messages/{{ id }}/replies"
-//             version="beta"
-//           >
-//             <template data-type="default">
-//               <div data-if="{{ value.length > 0 }}" class="teams-replies"></div>
-//             </template>
-//             <template data-type="value">
-//               <div
-//                 data-if="!deletedDateTime && messageType !== 'systemEventMessage'"
-//                 class="teams-content teams-reply"
-//               >
-//                 <div class="teams-message-reply-author-avatar">
-//                   <mgt-person
-//                     user-id="{{from.user.id}}"
-//                     avatar-size="large"
-//                     view="avatar"
-//                     person-card="hover"
-//                     show-presence="true"
-//                     class="mgt-person-medium"
-//                   >
-//                   </mgt-person>
-//                 </div>
-//                 <div class="teams-message-reply">
-//                   <mgt-person id="mgt-person-author" user-id="{{from.user.id}}">
-//                     <template type="default">
-//                       <span class="teams-message-author-title"
-//                         >{{ person.displayName }}
-//                       </span>
-//                       <span class="teams-message-date"
-//                         >{{ formatDate($parent.lastModifiedDateTime)}}</span
-//                       >
-//                     </template>
-//                   </mgt-person>
-//                   <div data-props="innerHTML: body.content"></div>
-//                 </div>
-//               </div>
-//             </template>
-//           </mgt-get>
-//         </div>
-//       </div>
-//     </template>
-//     <template data-type="loading">
-//       <fluent-progress-ring></fluent-progress-ring>
-//     </template>
-//   </mgt-get>
