1ee65ba074f0f30d0645a881b78faa13feb72af9
= <v.moskalenko@brightlab.me>
2023-03-29 15:55:13 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,108 @@
 /** @format */
 
-import { FluentThemeProvider, MessageThread } from "@azure/communication-react";
-import { Get, Login, MgtLogin, Providers } from "@microsoft/mgt-react";
-import { Chat } from "@microsoft/microsoft-graph-types";
+import {
+  FluentThemeProvider,
+  MessageStatus,
+  MessageThread,
+} from "@azure/communication-react";
+import {
+  Get,
+  Login,
+  MgtLogin,
+  MgtTemplateProps,
+  Providers,
+  SelectedChannel,
+  TeamsChannelPicker,
+} from "@microsoft/mgt-react";
+import { Chat, Message } from "@microsoft/microsoft-graph-types";
 import { FC, useState, useCallback } from "react";
 import ChatListTemplate from "./components/ChatListTemplate";
 import { Person, PersonCardInteraction } from "@microsoft/mgt-react";
 
 interface IApp {}
 
-const client = Providers;
-// console.log("client: ", client);
-
 const App: FC<IApp> = (props) => {
   const {} = props;
+  const [teamsId, setTeamsId] = useState<string>();
+  const [channelId, setChannelId] = useState<string>();
 
-  const [chatId, setChatId] = useState<string>();
+  const handleSelectedChannel = (
+    props: CustomEvent<SelectedChannel | null>
+  ) => {
+    setChannelId(props?.detail?.channel?.id);
+    setTeamsId(props?.detail?.team?.id);
+  };
 
-  const chatSelected = useCallback(
-    (e: Chat) => {
-      console.log("e: ", e);
-      setChatId(e.id);
-    },
-    [setChatId]
-  );
+  const HandleMessageAndPerson = (props: MgtTemplateProps) => {
+    const replies = props?.dataContext?.value as any[];
 
-  const HandleChat = (props: any) => {
-    const { value } = props?.dataContext;
+    if (!replies?.length) return null;
+    return replies?.map((reply) => {
+      return (
+        <div key={reply?.id} className={"ml-3"}>
+          {reply?.body?.content}
+        </div>
+      );
+    });
+  };
+
+  const RenderLastMessage = (props: MgtTemplateProps) => {
+    const messages = props?.dataContext?.value as Message[];
+    console.log("messages: ", messages);
+
     return (
-      <FluentThemeProvider>
-        <MessageThread
-          userId={"14658b23-6a98-4a79-8162-1c3653fc1f54"}
-          messages={value}
-          showMessageDate={true}
-          // disableEditing={chatState.disableEditing}
-          // TODO: Establish how to stop loading more messages and have it load more as part of an infinite scroll
-          // numberOfChatMessagesToReload={chatState.numberOfChatMessagesToReload}
-          // onLoadPreviousChatMessages={chatState.onLoadPreviousChatMessages}
-          // TODO: Messages date rendering is behind beta flag, find out how to enable it
-          // onDisplayDateTimeString={(date: Date) => date.toISOString()}
+      <>
+        {messages?.map((message: any) => {
+          return (
+            <div key={message?.id}>
+              <div>{message?.body?.content}</div>
+              <Get
+                resource={`teams/${message?.channelIdentity?.teamId}/channels/${message?.channelIdentity?.channelId}/messages/${message?.id}/replies?$top=1`}>
+                <HandleMessageAndPerson />
+              </Get>
+            </div>
+          );
+        })}
+      </>
+    );
+  };
 
-          // current behavior for re-send is a delete call with the clientMessageId and the a new send call
-          // onDeleteMessage={chatState.onDeleteMessage}
-          // onSendMessage={chatState.onSendMessage}
-          // onUpdateMessage={chatState.onUpdateMessage}
-          // render props
+  return (
+    <>
+      <Login></Login>
+      <div className="flex">
+        <div>
+          <TeamsChannelPicker selectionChanged={handleSelectedChannel} />
+          {teamsId && channelId && (
+            <Get
+              resource={`teams/${teamsId}/channels/${channelId}/messages`}
+              cacheEnabled={true}>
+              <RenderLastMessage />
+              {/* <ChatListTemplate template="default" onSelected={chatSelected} /> */}
+            </Get>
+          )}
+        </div>
+        <div className="w-full">
+          {/* {chatId && (
+            <Get resource={`/chats/${chatId}/messages`}>
+              <HandleChat />
+            </Get>
+          )} */}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
+
+{
+  /* <FluentThemeProvider>
+        <MessageThread
+          userId={""}
+          messages={messages}
+          showMessageDate={true}
           onRenderAvatar={(userId?: string) => {
-            console.log("userId=>", userId);
             return (
               <Person
                 userId={userId}
@@ -56,35 +112,5 @@ const App: FC<IApp> = (props) => {
             );
           }}
         />
-        {/* <SendBox
-              autoFocus="sendBoxTextField"
-              onSendMessage={chatState.onSendMessage}
-            /> */}
-        {/* <ErrorBar activeErrorMessages={chatState.activeErrorMessages} /> */}
-      </FluentThemeProvider>
-    );
-  };
-
-  return (
-    <>
-      <Login></Login>
-      <div className="flex">
-        <div>
-          <Get resource="me/chats" scopes={["chat.read"]} cacheEnabled={true}>
-            <ChatListTemplate template="default" onSelected={chatSelected} />
-          </Get>
-        </div>
-        <div className="w-full">
-          {chatId && (
-            <Get
-              resource={`/teams/{team-id}/channels/{channel-id}/threads/{thread-id}`}>
-              <HandleChat />
-            </Get>
-          )}
-        </div>
-      </div>
-    </>
-  );
-};
-
-export default App;
+      </FluentThemeProvider> */
+}
